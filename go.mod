b8fdc4f9dc60cb9686d55d7fc3a995f18118334c
Daniel Suarez <suarez31139@gmail.com>
2023-01-03 20:33:33 -0500

    inital commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/EstebanSL16/mycalculator2
+
+go 1.18
---
+++ mycalculator2.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Calc struct{}
+
+//Así quedó de la clase
+func (Calc) Operate(entrada string, operador string) int {
+	entradaLimpia := strings.Split(entrada, operador)
+	operador1 := parsear(entradaLimpia[0])
+	operador2 := parsear(entradaLimpia[1])
+	switch operador {
+	case "+":
+		fmt.Println(operador1 + operador2)
+		return operador1 + operador2
+	case "-":
+		fmt.Println(operador1 - operador2)
+		return operador1 - operador2
+	case "*":
+		fmt.Println(operador1 * operador2)
+		return operador1 * operador2
+	case "/":
+		fmt.Println(operador1 / operador2)
+		return operador1 / operador2
+	default:
+		fmt.Println(operador, "no está soportado")
+		return 0
+	}
+}
+
+func parsear(entrada string) int {
+	operador, _ := strconv.Atoi(entrada)
+	return operador
+}
+
+func LeerEntrada() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
